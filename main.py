be230468c90ebe11e80f3c6e8fdccda65ed5587f
Frank Ha <dqbui3295@gmail.com>
2022-06-05 10:34:04 +0700

    Update main.py to import and call functions from report_functions.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import os
+from datetime import datetime
+from report_functions import *
 
 
 NUM_STUDENTS = 1000
@@ -18,3 +20,36 @@ def load_report_card(directory, student_number):
         return {}
 
     return report_card
+
+
+report_list = []  # loading student report cards individually
+start_time = datetime.now()
+
+for idx in range(NUM_STUDENTS):
+    # print(f'Loading student: {idx}')
+    report_list.append(load_report_card('students', idx))
+
+end_time = datetime.now()
+time_taken = end_time - start_time
+
+print(f'Time taken to fetch: {time_taken}')
+print()
+
+# calculate required data summary metrics
+average_all_students = get_all_average(report_list)
+hardest_subject = get_hardest_subject(report_list)
+easiest_subject = get_easiest_subject(report_list)
+best_grade = get_best_grade(report_list)
+worst_grade = get_worst_grade(report_list)
+best_student = get_best_student(report_list)
+worst_student = get_worst_student(report_list)
+
+
+# display data
+print(f'Average Student Grade: {average_all_students}')
+print(f'Hardest Subject: {hardest_subject}')
+print(f'Easiest Subject: {easiest_subject}')
+print(f'Best Performing Grade: {best_grade}')
+print(f'Worst Performing Grade: {worst_grade}')
+print(f'Best Student ID: {best_student}')
+print(f'Worst Student ID: {worst_student}')
---
+++ report_functions.py
@@ -0,0 +1,23 @@
+def say_hi():
+    print('Hello world!!')
+
+
+def get_all_average(report_list): return 50
+
+
+def get_hardest_subject(report_list): return 'geo'
+
+
+def get_easiest_subject(report_list): return 'english'
+
+
+def get_best_grade(report_list): return 6
+
+
+def get_worst_grade(report_list): return 5
+
+
+def get_best_student(report_list): return 243
+
+
+def get_worst_student(report_list): return 420
